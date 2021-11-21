a85cbf23e628215071ec2502423551add88c7f69
jvicog <javiervico8@gmail.com>
2021-11-21 21:21:06 +0100

    Settings Hot Fix

--- HitOnTheBeat/Assets/Scripts/Settings/Brillo.cs
+++ HitOnTheBeat/Assets/Scripts/Settings/Brillo.cs
@@ -13,7 +13,7 @@ public class Brillo : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        slider.value = PlayerPrefs.GetFloat("brillo", 0.7f);
+        slider.value = PlayerPrefs.GetFloat("brillo", 0.5f);
         panelBrillo.color = new Color(panelBrillo.color.r, panelBrillo.color.g, panelBrillo.color.b, sliderValue);
         RevisarBrillo(slider.value);
     }
